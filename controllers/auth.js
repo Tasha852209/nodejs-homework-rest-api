d77b8a2e05c460396255f2f020d978b8e02be893
Tetiana Dub <112940506+Tasha852209@users.noreply.github.com>
2024-01-03 22:38:11 +0100

    added jimp

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,16 @@
 const { User } = require("../models/user");
-
 const bcrypt = require("bcrypt");
-
 const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
+const path = require("path");
+const Jimp = require("jimp");
+const fs = require("fs/promises");
 
 const { HttpError, cntrlWrapper } = require("../helpers");
 
 const { SECRET_KEY } = process.env;
+
+const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -17,7 +21,13 @@ const register = async (req, res) => {
 
   const hashPassword = await bcrypt.hash(password, 10);
 
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const avatarURL = gravatar.url(email);
+
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
 
   res.status(201).json({
     name: newUser.name,
@@ -63,9 +73,29 @@ const logout = async (req, res) => {
   });
 };
 
+const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  const { path: tempUpload, originalname } = req.file;
+
+  const filename = `${_id}_${originalname}`;
+  const resultUpload = path.join(avatarsDir, filename);
+  await fs.rename(tempUpload, resultUpload);
+  const image = await Jimp.read(resultUpload);
+  image.resize(250, 250).write(resultUpload);
+
+  const avatarURL = path.join("avatars", filename);
+  console.log(avatarURL);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.json({
+    avatarURL,
+  });
+};
+
 module.exports = {
   register: cntrlWrapper(register),
   login: cntrlWrapper(login),
   getCurrent: cntrlWrapper(getCurrent),
   logout: cntrlWrapper(logout),
+  updateAvatar: cntrlWrapper(updateAvatar),
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { validateBody, authenticate } = require("../../middlewares");
+const { validateBody, authenticate, upload } = require("../../middlewares");
 
 const { schemas } = require("../../models/user");
 
@@ -12,5 +12,11 @@ router.post("/register", validateBody(schemas.registerSchema), cntrl.register);
 router.post("/login", validateBody(schemas.loginSchema), cntrl.login);
 router.get("/current", authenticate, cntrl.getCurrent);
 router.post("/logout", authenticate, cntrl.logout);
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  cntrl.updateAvatar
+);
 
 module.exports = router;
